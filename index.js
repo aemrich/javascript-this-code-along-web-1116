5f44be9f39d0253574445d5d45739dd78811be4f
aemrich <adam.emrich@gmail.com>
2017-01-20 13:43:11 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much."
+
+function visitTable() {
+  console.log("The server is visiting " + this.name + " at table number " + this.tableNumber)
+}
+
+function Customer(name, tableNumber){
+  this.tableNumber = tableNumber
+  this.name = name
+}
+
+var sally = new Customer("sally", "4")
+
+
+var visitSally = visitTable.bind(sally)
+
+setTimeout(visitSally, 1000)
